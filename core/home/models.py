55d2f9977e4f5ac55d07a83c596fefa762c935c9
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-02-21 20:33:26 +0330

    class car foreignkey changed to manytomany

--- core/home/models.py
+++ core/home/models.py
@@ -11,7 +11,7 @@ class Person(models.Model):
 
 
 class Car(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    person = models.ManyToManyField(Person)
     name = models.CharField(max_length=100)
     year = models.IntegerField()
 
